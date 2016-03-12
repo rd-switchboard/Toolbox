321b12cd319d9474e361d4b15b0a1533b09204b6
Dmitrij Kudriavcev <dmitrij@kudriavcev.info>
2016-03-12 17:08:37 +1100

    Added Batch save function to DynamodDb test program

--- Test/test-dynamo-db/src/main/java/org/rdswitchboard/tests/performance/dynamo/db/App.java
+++ Test/test-dynamo-db/src/main/java/org/rdswitchboard/tests/performance/dynamo/db/App.java
@@ -1,6 +1,8 @@
 package org.rdswitchboard.tests.performance.dynamo.db;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
@@ -11,14 +13,18 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.document.BatchWriteItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.TableWriteItems;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.BatchWriteItemRequest;
 import com.amazonaws.services.dynamodbv2.model.DescribeLimitsRequest;
+import com.amazonaws.services.dynamodbv2.model.WriteRequest;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 
 public class App {
 	
-	private static final int TEST_ITEMS = 2000;
+	private static final int TEST_ITEMS = 2770;
 
 	public static void main(String[] args) {
 		App app = new App();
@@ -29,29 +35,31 @@ public class App {
 	
 		AmazonDynamoDB dynamo = new AmazonDynamoDBClient(new InstanceProfileCredentialsProvider());
 		dynamo.setRegion(Region.getRegion(Regions.US_WEST_2));
-		//DynamoDBMapper mapper = new DynamoDBMapper(dynamo);
+		DynamoDBMapper mapper = new DynamoDBMapper(dynamo);
 		
-		dynamo.describeLimits((DescribeLimitsRequest) new DescribeLimitsRequest().withGeneralProgressListener(new ProgressListener() {
+		/*dynamo.describeLimits((DescribeLimitsRequest) new DescribeLimitsRequest().withGeneralProgressListener(new ProgressListener() {
 
 			@Override
 			public void progressChanged(ProgressEvent event) {
 				System.out.println("Bytes: " + event.getBytes() + ", Transfered : " + event.getBytesTransferred());
 				
 			}
-		}));
+		}));*/
 		
 		testBenchmark(TEST_ITEMS);
-		testSaveDb(TEST_ITEMS, dynamo);
-		testLoadDb(TEST_ITEMS, dynamo);
-		testDeleteDb(TEST_ITEMS, dynamo);
+		testSaveBatchDb(TEST_ITEMS, mapper);
+		//testLoadDb(TEST_ITEMS, dynamo);
+		//testDeleteDb(TEST_ITEMS, dynamo);
 	}
 	
 	private void testBenchmark(int testN) {
 		long startTime = System.currentTimeMillis();
 		
-		for (int n = 0; n < testN; ++n)
+		List<Record> records = new ArrayList<Record>();
+		for (int n = 0; n < testN; ++n) 
 		{
-			new Record(n);
+			Record r = new Record(n);
+			records.add(r);
 		}
 		
 		long estimatedTime = System.currentTimeMillis() - startTime;		
@@ -66,6 +74,22 @@ public class App {
 			Record r = new Record(n);
 			r.save(dynamo);
 		}
+		
+		long estimatedTime = System.currentTimeMillis() - startTime;		
+		System.out.println("Saved " + testN + " records over " + estimatedTime + " ms. That is " + (double) estimatedTime / (double) testN + " ms per record.");
+	}
+	
+	private void testSaveBatchDb(int testN, DynamoDBMapper mapper) {
+		long startTime = System.currentTimeMillis();
+
+		List<Record> records = new ArrayList<Record>();
+		for (int n = 0; n < testN; ++n) 
+		{
+			Record r = new Record(n);
+			records.add(r);
+		}
+
+		mapper.batchSave(records);
 		
 		long estimatedTime = System.currentTimeMillis() - startTime;		
 		System.out.println("Saved " + testN + " records over " + estimatedTime + " ms. That is " + (double) estimatedTime / (double) testN + " ms per record.");
--- Test/test-dynamo-db/src/main/java/org/rdswitchboard/tests/performance/dynamo/db/Record.java
+++ Test/test-dynamo-db/src/main/java/org/rdswitchboard/tests/performance/dynamo/db/Record.java
@@ -6,12 +6,19 @@ import java.util.Map;
 import java.util.Random;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.KeyAttribute;
+import com.amazonaws.services.dynamodbv2.document.PrimaryKey;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.DeleteItemRequest;
 import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
 import com.amazonaws.services.dynamodbv2.model.GetItemResult;
 import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 
+@DynamoDBTable(tableName="Test1M")
 public class Record {
 	private String nodeId;
 	private String text1;
@@ -39,42 +46,72 @@ public class Record {
 		return new BigInteger(130, random).toString(32);
 	}
 	
+	@DynamoDBHashKey(attributeName="NodeID")
 	public String getNodeId() {
 		return nodeId;
 	}
 	public void setNodeId(String nodeId) {
 		this.nodeId = nodeId;
 	}
+	
+	@DynamoDBAttribute(attributeName="Text1")
 	public String getText1() {
 		return text1;
 	}
 	public void setText1(String text1) {
 		this.text1 = text1;
 	}
+	
+	@DynamoDBAttribute(attributeName="Text2")
 	public String getText2() {
 		return text2;
 	}
 	public void setText2(String text2) {
 		this.text2 = text2;
 	}
+	
+	@DynamoDBAttribute(attributeName="Text3")
 	public String getText3() {
 		return text3;
 	}
 	public void setText3(String text3) {
 		this.text3 = text3;
 	}
+	
+	@DynamoDBAttribute(attributeName="Text4")
 	public String getText4() {
 		return text4;
 	}
 	public void setText4(String text4) {
 		this.text4 = text4;
 	}
+	
+	@DynamoDBAttribute(attributeName="Text5")
 	public String getText5() {
 		return text5;
 	}
 	public void setText5(String text5) {
 		this.text5 = text5;
 	}
+
+	public PrimaryKey getKey() {
+		return new PrimaryKey()
+				.addComponents(
+						new KeyAttribute("NodeID", 
+								new AttributeValue()
+								.withS(nodeId)));
+	}
+	
+	public Item getItem() {
+		return new Item()
+				.withPrimaryKey(getKey())
+				.withString("text1", text1)
+				.withString("text2", text2)
+				.withString("text3", text3)
+				.withString("text4", text4)
+				.withString("text5", text5);
+	}
+	
 	
 	public void save(AmazonDynamoDB dynamo) {
 	    Map<String, AttributeValue> item = new HashMap<String, AttributeValue>();
