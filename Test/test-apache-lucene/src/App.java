e54d776df958e3b9fa663396b202791a4c5398b8
Amir Aryani <amir.aryani@gmail.com>
2016-03-28 19:40:02 +1100

    added a test program for fuzzy search using Apache Lucene

---
+++ Test/test-apache-lucene/src/App.java
@@ -0,0 +1,15 @@
+
+import java.io.IOException;
+import org.apache.lucene.queryparser.classic.ParseException;
+
+
+public class App {
+	public static void main(String[] args) throws IOException, ParseException {
+
+		SingleSearch singleSearchObj= new SingleSearch();
+		singleSearchObj.test("Manage Gigabyte");
+		
+    }
+
+
+}
