9789c2e0d452f08bf110860b53f0d23aa9a1dac3
Amir Aryani <amir.aryani@gmail.com>
2016-03-28 20:20:04 +1100

    added a single line comment

--- Test/test-apache-lucene/src/App.java
+++ Test/test-apache-lucene/src/App.java
@@ -6,6 +6,7 @@ import org.apache.lucene.queryparser.classic.ParseException;
 public class App {
 	public static void main(String[] args) throws IOException, ParseException {
 
+		//Test search in an array of strings
 		SingleSearch singleSearchObj= new SingleSearch();
 		singleSearchObj.test("Manage Gigabyte");
 		
